3260cf07e658286b1895846b5a20cea5f7c59dc1
princeBarimene <prince.g.olubari@gmail.com>
2024-11-23 21:47:21 +0000

    created a canvas and added an image with padding

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
+window.config(padx=20,pady=20)
+
+canvas = Canvas(width=200, height = 200)
 
 
+
+lock_img = PhotoImage(file="./logo.png")
+canvas.create_image(100,100, image = lock_img)
+canvas.pack()
 
 
 
