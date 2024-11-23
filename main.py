f434ad00ddc2320e19d05aa1a5fae3bdef432095
princeBarimene <prince.g.olubari@gmail.com>
2024-11-23 22:12:33 +0000

    added 3 labels

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
+# ---------------------------- CONSTANTS ------------------------------- #
+
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -17,8 +20,18 @@ canvas = Canvas(width=200, height = 200)
 
 lock_img = PhotoImage(file="./logo.png")
 canvas.create_image(100,100, image = lock_img)
-canvas.pack()
+canvas.grid(column=1, row=0)
 
+
+#create labels
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
 
 
 
