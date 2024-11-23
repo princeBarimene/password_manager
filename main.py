81611a1dda2a964855f27f1bbae23582ed9309a6
princeBarimene <prince.g.olubari@gmail.com>
2024-11-23 21:33:15 +0000

    window set up done

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+
+
+
+
+
+
+window.mainloop()
+
+
