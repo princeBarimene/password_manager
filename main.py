8e8078633535a1ab2e75964abe524f0d51091633
princeBarimene <prince.g.olubari@gmail.com>
2024-11-23 22:55:55 +0000

    layout complete with labels, entries and buttons. Dimensions were tested to suit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20,pady=20)
+window.config(padx=50,pady=50)
 
 canvas = Canvas(width=200, height = 200)
 
@@ -32,6 +32,28 @@ email_label.grid(column=0, row=2)
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
+
+
+#create inputs
+website_input = Entry(width=45)
+website_input.grid(column=1, row=1, columnspan=2)
+
+email_input = Entry(width=45)
+email_input.grid(column=1, row=2, columnspan=2)
+
+password_input = Entry(width=26)
+password_input.grid(column=1, row=3)
+
+
+#creating buttons
+
+generate_button = Button(text="Generate Password")
+generate_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=42)
+add_button.grid(column=1, row=4, columnspan=2)
+
+
 
 
 
